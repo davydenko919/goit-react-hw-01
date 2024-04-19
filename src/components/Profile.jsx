6a2cbe66c22a3af69522191345e3f6fc0549a397
davydenko919 <davydenko919@gmail.com>
2024-04-19 22:53:58 +0300

    T1

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,30 +1,36 @@
-export default function Profile(){
-    return (
-        <div>
-        <div>
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-            alt="User avatar"
-          />
-          <p>Petra Marica</p>
-          <p>@pmarica</p>
-          <p>Salvador, Brasil</p>
-        </div>
-      
-        <ul>
-          <li>
-            <span>Followers</span>
-            <span>1000</span>
-          </li>
-          <li>
-            <span>Views</span>
-            <span>2000</span>
-          </li>
-          <li>
-            <span>Likes</span>
-            <span>3000</span>
-          </li>
-        </ul>
+export default function Profile({
+    username,
+    tag,
+    location,
+    avatar,
+    stats: { followers, views, likes },
+}) {
+  return (
+    <div>
+      <div>
+        <img
+          src={avatar}
+          alt="User avatar"
+        />
+        <p>{username}</p>
+        <p>@{tag}</p>
+        <p>{location}</p>
       </div>
-    );
-  }
+
+      <ul>
+        <li>
+          <span>Followers</span>
+          <span>1000</span>
+        </li>
+        <li>
+          <span>Views</span>
+          <span>2000</span>
+        </li>
+        <li>
+          <span>Likes</span>
+          <span>3000</span>
+        </li>
+      </ul>
+    </div>
+  );
+}
