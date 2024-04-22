5a7f69e17dbdfd36f726e56750c5b7fc4ab0e99a
davydenko919 <davydenko919@gmail.com>
2024-04-22 17:59:03 +0300

    task3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,8 @@ import Profile from './Profile.jsx';
 import userData from "../userData.json";
 import FriendList from './FriendList.jsx';
 import friends from "../friends.json";
+import TransactionHistory from './TransactionHistory.jsx';
+import transactions from "../transactions.json";
 
 export default function App() {
   return (
@@ -14,6 +16,7 @@ export default function App() {
         stats={userData.stats}
       />
       <FriendList friends={friends} /> 
+      <TransactionHistory items={transactions} />
     </>
   );
 }
